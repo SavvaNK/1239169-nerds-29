65d22d5132b10b421dc06c1f90701f9877049a95
Savva Kirichenko <savvakn@gmail.com>
2020-11-09 00:51:17 +0300

    to check 2

--- js/script.js
+++ js/script.js
@@ -23,8 +23,10 @@ for (let i = 0; i < btnList.length; i += 1) {
 const modalOpenBtn = document.querySelector(".map-contacts-button");
 const modalCloseBtn = document.querySelector(".modal-close");
 const modal = document.querySelector(".modal");
+const form = modal.querySelector(".modal-form");
 const name = modal.querySelector("[name=name]");
 const email = modal.querySelector("[name=email]");
+const text = modal.querySelector("[name=text]");
 
 modalOpenBtn.addEventListener("click", function (evt) {
   evt.preventDefault();
@@ -32,18 +34,68 @@ modalOpenBtn.addEventListener("click", function (evt) {
   name.focus();
 });
 
+const closeModal = () => {
+  modal.classList.add("modal-close-animation");
+  setTimeout(function() {
+    modal.classList.remove("modal-show", "modal-close-animation");
+    name.classList.remove("modal-form-input-invalid");
+    email.classList.remove("modal-form-input-invalid");
+    text.classList.remove("modal-form-input-invalid");
+  }, 500);
+}
+
 modalCloseBtn.addEventListener("click", function (evt) {
   evt.preventDefault();
-  modal.classList.remove("modal-show");
+  closeModal();
 });
 
 window.addEventListener("keydown", function (evt) {
   if (evt.key === "Escape") {
     if (modal.classList.contains("modal-show")) {
       evt.preventDefault();
-      modal.classList.remove("modal-show");
-      // modal.classList.remove("modal-error");
+      closeModal();
     }
+  }
+});
+
+const containsEmptyValue = function() {
+  if (!this.value) {
+    this.classList.add("modal-form-input-invalid");
+  }
+};
+
+const removeInvalidClass = function() {
+  if (this.classList.contains("modal-form-input-invalid")) {
+    this.classList.remove("modal-form-input-invalid");
+  }
+};
+
+name.onblur = containsEmptyValue;
+name.onfocus = removeInvalidClass;
+
+email.onblur = function() {
+  if (!this.value.includes("@")) {
+    this.classList.add("modal-form-input-invalid");
+  }
+};
+email.onfocus = removeInvalidClass;
+
+text.onblur = containsEmptyValue;
+text.onfocus = removeInvalidClass;
+
+// silliest form validation, but whatever, I just want to shake modal XD
+const isFormValid = () => (
+    name.value
+    && email.value.includes("@")
+    && text.value
+);
+
+form.addEventListener("submit", function (evt) {
+  if (!isFormValid()) {
+    evt.preventDefault();
+    modal.classList.remove("modal-error");
+    // modal.offsetWidth = modal.offsetWidth;
+    modal.classList.add("modal-error");
   }
 });
 
